436cd3d503c4599e5ae690f366295d8c235e5119
AnaaCristinaa <anaacristinaa96@gmail.com>
2017-12-18 23:07:19 +0000

    alterei a função main

--- main_image_recognition.m
+++ main_image_recognition.m
@@ -1,14 +1,16 @@
 %% Se receber apenas 1 parâmetro, o param1 é o path para a imagem
 
-function [gray_im] = main_image_recognition(param1,param2)
+function [img_out] = main_image_recognition(param1,param2,param3,param4)
 
     switch(nargin)
         case 1
             img = imread(param1);
-            gray_im = rgb2gray(img);
-        case 2
+            img_out = rgb2gray(img);
+        case 3
+            img_out = imnoise(param1,param2,param3);
+        case 4
+            img_out = imnoise(param1,param2,param3,param4);
             
-            
+
     end
 end
-
