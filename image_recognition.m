ab4dafdd2eca790298fed24b9f5cf3611f1aa155
pauloaferreiraa <pauloazevedoferreiraa@gmail.com>
2017-12-19 23:41:27 +0000

    Merge

--- image_recognition.m
+++ image_recognition.m
@@ -5,4 +5,5 @@ gray_img = main_image_recognition('Images/coins.jpg');
 %% 
 gaussian_noise = 'gaussian';
 salt_pepper_noise = 'salt & pepper';
-main_image_recognition(gray_img,gaussian_noise);
+img = main_image_recognition(gray_img,gaussian_noise,0.001,0.01);
+imshow(img);
